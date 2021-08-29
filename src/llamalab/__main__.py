bda1066ff0c64175b347b90a7fede2a421f55b96
tory1103 <adrian.rubiboy11@gmail.com>
2021-08-29 15:57:44 +0200

    Updates llamalab

--- src/llamalab/__main__.py
+++ src/llamalab/__main__.py
@@ -1,4 +1,6 @@
 from fire import Fire
 from connections import Cloud
+from documentation import *
+from fast_http import simple_server
 
 if __name__ == '__main__': Fire()
--- src/tests/connections-send-test.py
+++ src/tests/connections-send-test.py
@@ -7,6 +7,7 @@ Functions used:
 Issues: https://github.com/SerTor-Automate/llamalab/issues
 
 """
+
 from src.llamalab.connections import Cloud
 
 with Cloud.send("secret", "test@gmail.com", "payload_message") as response: print(response.text)
